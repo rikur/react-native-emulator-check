3f57d86801710c3408d7fdde7de2aa26c752b0e9
tarouboy <kevinkev@gmail.com>
2020-04-20 12:06:14 +0800

    update emulator check

--- android/src/main/java/com/github/wusuopu/RNEmulatorCheck/RNEmulatorCheckModule.java
+++ android/src/main/java/com/github/wusuopu/RNEmulatorCheck/RNEmulatorCheckModule.java
@@ -12,14 +12,24 @@ import java.util.Map;
  * Created by lcj on 9/20/16.
  */
 public class RNEmulatorCheckModule extends ReactContextBaseJavaModule {
-    private static Boolean isEmulator = Build.FINGERPRINT.startsWith("generic")
+    private boolean isEmulator() {
+        return (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+            || Build.FINGERPRINT.startsWith("generic")
             || Build.FINGERPRINT.startsWith("unknown")
+            || Build.HARDWARE.contains("goldfish")
+            || Build.HARDWARE.contains("ranchu")
             || Build.MODEL.contains("google_sdk")
             || Build.MODEL.contains("Emulator")
             || Build.MODEL.contains("Android SDK built for x86")
             || Build.MANUFACTURER.contains("Genymotion")
-            || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
-            || "google_sdk".equals(Build.PRODUCT);
+            || Build.PRODUCT.contains("sdk_google")
+            || Build.PRODUCT.contains("google_sdk")
+            || Build.PRODUCT.contains("sdk")
+            || Build.PRODUCT.contains("sdk_x86")
+            || Build.PRODUCT.contains("vbox86p")
+            || Build.PRODUCT.contains("emulator")
+            || Build.PRODUCT.contains("simulator");
+    }
 
     public RNEmulatorCheckModule(ReactApplicationContext reactContext) {
         super(reactContext);
